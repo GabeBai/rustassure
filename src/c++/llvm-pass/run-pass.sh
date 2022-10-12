d2952edeb0775847d92639c9348716ed6b94cb2d
Hamed <sghavamnia@cs.stonybrook.edu>
2022-10-11 23:48:18 -0400

    extract required information from llvm pass

--- src/c++/llvm-pass/run-pass.sh
+++ src/c++/llvm-pass/run-pass.sh
@@ -13,8 +13,12 @@ input=$1
 while read line; do
 # reading each line
     val=$(echo $line|tr '\n' ' ')
-    filename=$(basename $val)
+    filename=$(basename $val | sed 's/\.so\..*\.preopt\.bc/\.so/g')
+    echo $filename
     echo "running Rustifer for $val"
+    set -x
 #   ./Debug-build/bin/rustifier -libc-func-file /tmp/libc.exported $val -enable-cve -enable-debugging -print-easy-funcs > outputs/$filename.out;
-   ./Debug-build/bin/rustifier $val -enable-debugging -print-arg-type-strs > outputs/$filename.out;
+   ./Debug-build/bin/rustifier $val -enable-debugging -rustify-enable-pta -enable-api-based -export-func-list /home/hamed/rust/rustify/src/python/outputs/$filename-exports.out > logs/$filename.log;
+    cat logs/$filename.log | grep '^Total Func' | sed "s/^Total/$filename: Total/g" >> logs/funcs.out
+    set +x
 done < $input
