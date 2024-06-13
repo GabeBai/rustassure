06d9e39123f16d11975dba646ee0fb3e19e845fb
Tapti Palit <tpalit@purdue.edu>
2024-06-13 11:04:12 -0400

    Refactor GPT access code (chunking/chaining/etc) into a separate translation module

---
+++ src/python/translation.py
@@ -0,0 +1,122 @@
+import os
+import logging
+import sys
+import re
+import glob
+from openai import OpenAI
+import subprocess
+import traceback
+import tiktoken
+
+
+class Translator:
+    """
+    https://platform.openai.com/docs/guides/text-generation/chat-completions-api
+    """
+    def __init__(self, logger, baseUrl, apiKey, ctxWindow, maxCompletionTokens,
+            srcLang, dstLang,
+            model, systemPrompt):
+        self.logger = logger
+        self.baseUrl = baseUrl
+        self.apiKey = apiKey
+        self.ctxWindow = ctxWindow
+        self.maxCompletionTokens = maxCompletionTokens
+        self.requestTokenLimit = self.ctxWindow - maxCompletionTokens # Should this only be the ctxWindow?
+        self.srcLang = srcLang
+        self.dstLang = dstLang
+        self.model = model
+        self.systemPrompt = systemPrompt
+        # If we're using LLAMA then we need to provide self.baseUrl
+        # self.client = OpenAI(base_url=self.baseUrl, api_key=self.apiKey)
+        self.client = OpenAI(api_key=self.apiKey)
+
+    def isResponseTruncated(self, completion, funcName):
+        finishReason = completion.choices[0].finish_reason
+        self.logger.debug("Finish reason for function %s: %s", funcName, finishReason)
+        return finishReason == "length"
+
+    def getResponse(self, request):
+        # self.logger.debug("Sending request: %s", request)
+        completion = self.client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": self.systemPrompt},
+                {"role": "user", "content": request}], 
+            max_tokens = self.maxCompletionTokens,
+            temperature = 0.2,
+            top_p = 0.1)
+        response = completion.choices[0].message.content
+        # This is OpenAI specific
+        # Try to remove the ```rust at the first line and ``` at the last line that I think indicates formatting (markdown?)
+        responseLines = response.split("\n")
+        if "rust" in responseLines[0]:
+            responseLines = responseLines[1:]
+        if "```" in responseLines[-1]:
+            responseLines = responseLines[:-1]
+        response = "\n".join(responseLines)
+        return (completion, response)
+
+    def send(self, funcName, request): 
+        (completion, response) = self.getResponse(request)
+        # self.logger.debug("response = %s", completion)
+
+        # We might have to do continuation and chaining
+        # Note that the context window is different from the maximum
+        # number of output tokens GPT can return at one go
+        # The context window, for e.g., is 16K for GPT-3.5, but maximum 
+        # number of completion tokens is 4K.
+        # So it might still have the input in its context, when
+        # it truncated its output
+        chainedResponse = response
+
+        # Keep on telling it to continue till the response is no
+        # longer truncated
+        while self.isResponseTruncated(completion, funcName):
+            """
+            continuationPrompt = response.split()[0-CONTINUATION_PROMPT_LEN:]
+            continuationPrompt = ' '.join(continuationPrompt)
+            """
+            continuationPrompt = "continue"
+            (completion, response) = self.getResponse(continuationPrompt)
+            chainedResponse = chainedResponse + response
+        return chainedResponse
+
+    def countTokens(self, line):
+        tokenizer = tiktoken.get_encoding(tiktoken.encoding_name_for_model(self.model))
+        tokens = tokenizer.encode(line)
+        return len(tokens)
+    
+    def chunkAndSend(self, funcName, request):
+        totalTokens = self.countTokens(request)
+        fullResponse = ""
+        numChunks = 1
+        if totalTokens > self.requestTokenLimit:
+            lines = request.split("\n")
+            # Get the next chunk
+            nextLineIndex = 0
+            while nextLineIndex < len(lines):
+                # Build a chunk
+                numTokensSoFar = 0
+                chunk = ""
+                # Keep consuming the next line as long as consuming the next line wouldn't go over the limit
+                nextLineTokenCount = self.countTokens(lines[nextLineIndex])
+                while numTokensSoFar + nextLineTokenCount < self.requestTokenLimit and nextLineIndex < len(lines):
+                    chunk = chunk + '\n' + lines[nextLineIndex]
+                    nextLineIndex = nextLineIndex + 1
+                    numTokensSoFar = numTokensSoFar + nextLineTokenCount
+                # We've built a chunk! Let's send it
+                response = self.send(funcName, chunk)
+                fullResponse = fullResponse + response
+                numChunks = numChunks + 1
+        else:
+            fullResponse = self.send(funcName, request)
+        self.logger.info("Sent request in %d chunks", numChunks)
+        return fullResponse
+
+    def translate(self, funcName, funcSrc):
+        # self.logger.debug("Translating: %s",funcSrc)
+        request = "Translate " + self.srcLang + " to " + self.dstLang + ". The C source code might be chunked across different requests. Please don't end the function. Also DO NOT reply with anything other than the Rust code. No English words needed.\n"  + funcSrc
+        result = self.chunkAndSend(funcName, request)
+        return result
+
+
--- src/python/translationValidator.py
+++ src/python/translationValidator.py
@@ -6,9 +6,14 @@ import glob
 from openai import OpenAI
 import subprocess
 import traceback
+import tiktoken
 
+from translation import Translator
 
-TOKEN_TO_WORD_FACTOR = 0.75
+GPT_MODEL="gpt-3.5-turbo"
+CTX_WINDOW_LEN=16*1024
+MAX_COMPLETION_TOKENS=4096 # This is the max value you can put for max_tokens: the max size of a response, https://platform.openai.com/docs/models/gpt-4-turbo-and-gpt-4 and search for output tokens
+
 CONTINUATION_PROMPT_LEN = 200 # 200 chars
 
 class Range:
@@ -171,115 +176,6 @@ class FunctionAndDepsExtractor:
             # self.logger.info(code)
         return funcMap
 
-class Translator:
-    """
-    https://platform.openai.com/docs/guides/text-generation/chat-completions-api
-    """
-    def __init__(self, logger, baseUrl, apiKey, ctxWindow, maxCompletionTokens,
-            srcLang, dstLang,
-            model, systemPrompt):
-        self.logger = logger
-        self.baseUrl = baseUrl
-        self.apiKey = apiKey
-        self.ctxWindow = ctxWindow
-        self.maxCompletionTokens = maxCompletionTokens
-        self.requestTokenLimit = self.ctxWindow - maxCompletionTokens - 2048
-        self.maxChunkSizeWords = int(self.requestTokenLimit * TOKEN_TO_WORD_FACTOR)
-        self.srcLang = srcLang
-        self.dstLang = dstLang
-        self.model = model
-        self.systemPrompt = systemPrompt
-        # If we're using LLAMA then we need to provide self.baseUrl
-        # self.client = OpenAI(base_url=self.baseUrl, api_key=self.apiKey)
-        self.client = OpenAI(api_key=self.apiKey)
-
-    def estimateTokens(self, funcSrc):
-        words = funcSrc.split()
-        tokens = int(float(len(words))/TOKEN_TO_WORD_FACTOR)
-        return tokens
-
-    def isResponseTruncated(self, completion, funcName):
-        finishReason = completion.choices[0].finish_reason
-        self.logger.debug("Finish reason for function %s: %s", funcName, finishReason)
-        return finishReason == "length"
-
-    def getResponse(self, request):
-        # self.logger.debug("Sending request: %s", request)
-        completion = self.client.chat.completions.create(
-            model=self.model,
-            messages=[
-                {"role": "system", "content": self.systemPrompt},
-                {"role": "user", "content": request}], 
-            max_tokens = self.maxCompletionTokens,
-            temperature = 0.2,
-            top_p = 0.1)
-        response = completion.choices[0].message.content
-        # This is OpenAI specific
-        # Try to remove the ```rust at the first line and ``` at the last line that I think indicates formatting (markdown?)
-        responseLines = response.split("\n")
-        if "rust" in responseLines[0]:
-            responseLines = responseLines[1:]
-        if "```" in responseLines[-1]:
-            responseLines = responseLines[:-1]
-        response = "\n".join(responseLines)
-        return (completion, response)
-
-    def send(self, funcName, request): 
-        (completion, response) = self.getResponse(request)
-        # self.logger.debug("response = %s", completion)
-
-        # We might have to do continuation and chaining
-        # Note that the context window is different from the maximum
-        # number of output tokens GPT can return at one go
-        # The context window, for e.g., is 16K for GPT-3.5, but maximum 
-        # number of completion tokens is 4K.
-        # So it might still have the input in its context, when
-        # it truncated its output
-        chainedResponse = response
-
-        # Keep on telling it to continue till the response is no
-        # longer truncated
-        while self.isResponseTruncated(completion, funcName):
-            continuationPrompt = response.split()[0-CONTINUATION_PROMPT_LEN:]
-            continuationPrompt = ' '.join(continuationPrompt)
-            (completion, response) = self.getResponse(continuationPrompt)
-            chainedResponse = chainedResponse + response
-        return chainedResponse
-    
-    def chunkAndSend(self, funcName, request):
-        tokenEstimate = self.estimateTokens(request)
-        fullResponse = ""
-        numChunks = 1
-        if tokenEstimate > self.requestTokenLimit:
-            lines = request.split("\n")
-            # Get the next chunk
-            nextLineIndex = 0
-            while nextLineIndex < len(lines):
-                # Build a chunk
-                numWordsSoFar = 0
-                chunk = ""
-                # Keep consuming the next line as long as consuming the next line wouldn't go over our
-                # computed maxChunkSizeWords and we haven't consumed the entire file
-                nextLineWordCount = len(lines[nextLineIndex].split())
-                while numWordsSoFar + nextLineWordCount < self.maxChunkSizeWords and nextLineIndex < len(lines):
-                    chunk = chunk + '\n' + lines[nextLineIndex]
-                    nextLineIndex = nextLineIndex + 1
-                    numWordsSoFar = numWordsSoFar + nextLineWordCount
-                # We've built a chunk! Let's send it
-                response = self.send(funcName, chunk)
-                fullResponse = fullResponse + response
-                numChunks = numChunks + 1
-        else:
-            fullResponse = self.send(funcName, request)
-        self.logger.info("Sent request in %d chunks", numChunks)
-        return fullResponse
-
-    def translate(self, funcName, funcSrc):
-        # self.logger.debug("Translating: %s",funcSrc)
-        request = "Translate " + self.srcLang + " to " + self.dstLang + ". The C source code might be chunked across different requests. Please don't end the function. Also DO NOT reply with anything other than the Rust code. No English words needed.\n"  + funcSrc
-        result = self.chunkAndSend(funcName, request)
-        return result
-
 def getLogger(logPath):
     if os.path.exists(logPath):
         os.remove(logPath)
@@ -310,11 +206,11 @@ def createTranslator(logger):
     translator = Translator(logger,
             "http://172.31.224.1:12345/v1",
             os.environ.get('OPENAI_KEY'),
-            16*1024,
-            4096, # This is the max value you can put for max_tokens: the max size of a response, https://platform.openai.com/docs/models/gpt-4-turbo-and-gpt-4 and search for output tokens
+            CTX_WINDOW_LEN,
+            MAX_COMPLETION_TOKENS, 
             "C",
             "Rust",
-            "gpt-3.5-turbo",
+            GPT_MODEL,
             "You are an expert programmer in C and Rust and are an expert in translating C to Rust code.")
     return translator
 
@@ -393,13 +289,12 @@ def processCodebase(codebasePath, execPath):
         funcMap = getFunctions(logger, extractor, execPath, codebasePath)
         logger.debug("Extracted %d functions", len(funcMap))
         individualFuncPath = codebasePath+"/individual-funcs/"
-        try:
-            os.mkdir(individualFuncPath)
-        except:
-            logger.debug("Individual functions directory already exists")
+        os.mkdir(individualFuncPath)
         for key in funcMap:
             translateAndCreateIndividualFiles(translator, funcMap, key, logger, individualFuncPath)
-    emitLLVMBitcodes("./inputs-complex/zlib-1.3.1/", logger)
+    else:
+        logger.warn("Individual functions directory already exists, skipping regeneration")
+    emitLLVMBitcodes(codebasePath, logger)
     """
     translator = createTranslator(logger)
     with open("./inputs-complex/zlib-1.3.1/crc32.i") as f:
