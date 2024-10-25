9ad08b485abeefdf45f4c7fa8df5333fa9d1d4ba
Gabe Bai <gabbai@ucdavis.edu>
2024-10-24 22:09:54 -0700

    fix : fix cmake error

--- src/Symbolizer/build.sh
+++ src/Symbolizer/build.sh
@@ -3,5 +3,5 @@
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../ ;
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_STANDARD=17 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ ../ ;
 make ;
