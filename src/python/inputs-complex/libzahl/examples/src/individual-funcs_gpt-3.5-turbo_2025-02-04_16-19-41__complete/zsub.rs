c97fb0aa6eee6001a5f14f97a5cc84bcb6eda1a2
Gabe Bai <yubobai38@gmail.com>
2025-02-21 16:03:50 -0800

    Ignore inputs-complex

--- src/python/inputs-complex/libzahl/examples/src/individual-funcs_gpt-3.5-turbo_2025-02-04_16-19-41__complete/zsub.rs
+++
@@ -1,3 +0,0 @@
-#![allow(unaligned_references)]
-   use libc::{c_int, c_void};
-   
--- src/python/inputs-complex/zlib-1.3.1/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-make clean && make distclean
-CC=../clang-wrapper.sh CFLAGS="-O0" ./configure
-make
