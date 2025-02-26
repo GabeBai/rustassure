6168f35657245112d9cd1a5259eb516de4561704
Gabe Bai <yubobai38@gmail.com>
2025-02-26 10:25:58 -0800

    feat : remove unused file

--- src/python/inputs-complex/libcsv-csv_fwrite-orig.c
+++
@@ -1,7 +0,0 @@
-
-
-int
-csv_fwrite (FILE *fp, const void *src, size_t src_size)
-{
-  return csv_fwrite2(fp, src, src_size, CSV_QUOTE);
-}
--- src/python/inputs-complex/libcsv-csv_write-orig.c
+++
@@ -1,7 +0,0 @@
-
-
-size_t
-csv_write (void *dest, size_t dest_size, const void *src, size_t src_size)
-{
-  return csv_write2(dest, dest_size, src, src_size, CSV_QUOTE);
-}
