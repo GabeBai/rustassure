c60a7358968a44557557d39b1f965d1a4e5784ea
Gabe Bai <yubobai38@gmail.com>
2025-03-13 10:15:35 -0700

    feat : add terminate calculate

--- src/Symbolizer/Pass/SymbolizerPass.cpp
+++ src/Symbolizer/Pass/SymbolizerPass.cpp
@@ -270,7 +270,6 @@ namespace {
 			"core::result::Result<T,E>::expect",
 			"core::slice::<impl [T]>::is_empty",
 			"core::result::Result<T,E>::ok",
-			"core::ptr::metadata::from_raw_parts_mut",
 			// interesting, actually we has this function, but cannot get result with it..see good case..
 			"alloc::slice::<impl [T]>::into_vec",
 			"core::result::Result<T,E>::unwrap_or",
