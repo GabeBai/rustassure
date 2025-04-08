297cdce2433ca37ee29589245451d1d0859ed969
Gabe Bai <yubobai38@gmail.com>
2025-04-07 17:36:00 -0700

    feat : update symbolized steps

--- src/python/symbolicExecution.py
+++ src/python/symbolicExecution.py
@@ -255,22 +255,22 @@ def process_rust_file(bc_file):
 
     # demangle
     demangle_opt = (
-        f"opt -load-pass-plugin ../build/Pass/DemanglePass.so "
-        f"-O0 {bc_file} -S -o klee_ir_files/Rust/{base_name}_klee.ll"
+        f"opt -load-pass-plugin ../build/DemanglePass/DemanglePass.so "
+        f"-O0 {bc_file} -S -o klee_ir_files/Rust/{base_name}.ll"
     )
     run_command(demangle_opt)
 
     # link core
     link_core = (
         f"llvm-link klee_ir_files/Rust/{base_name}_klee.ll ../scripts/core_demangle.ll "
-        f"-S -o klee_ir_files/Rust/{base_name}_klee.ll"
+        f"-S -o klee_ir_files/Rust/{base_name}.ll"
     )
     run_command(link_core)
 
     # symbolize
     cmd_opt1 = (
         f"opt -load-pass-plugin ../build/Pass/SymbolizerPass.so "
-        f"-O0 klee_ir_files/Rust/{base_name}_klee.ll -S -o klee_ir_files/Rust/{base_name}_klee.ll"
+        f"-O0 klee_ir_files/Rust/{base_name}.ll -S -o klee_ir_files/Rust/{base_name}_klee.ll"
     )
     run_command(cmd_opt1)
 
