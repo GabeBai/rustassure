2fdfcfb91b3519ef5af1548aa127fd0b9f612d94
Gabe <yubobai38@gmail.com>
2025-04-13 07:24:45 +0000

    feat : add shuffle script

--- src/python/shuffle.py
+++ src/python/shuffle.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+impot sys
 
 from sympy import false
 
@@ -20,20 +21,28 @@ def find_i_files(base_dir):
 
     return i_files
 
-def move_matching_files(dest_dir, base_dir, i_files, except_ir = false):
+def move_matching_files(original_dest_dir, base_dir, i_files, except_ir = false, source = ""):
 
     for rel_path in i_files:
         for file in os.listdir(base_dir):
             if except_ir:
-                if file.startswith(rel_path) and not file.endswith(".ll"):
+                if file.startswith(rel_path) and not file.endswith(".ll") and not file.endswith(".bc"):
                     src_file = os.path.join(base_dir, file)
-                    dest_dir = os.path.join(dest_dir, rel_path, file)
-                    shutil.move(src_file, dest_dir)
+                    dest_dir = os.path.join(original_dest_dir, rel_path, file + "_" + source)
+                    if os.path.isdir(src_file):
+                        shutil.copytree(src_file, dest_dir)
+                    else:
+                        shutil.copy2(src_file, dest_dir)
+                    print(f"{src_file} to {dest_dir} finish")
             else:
                 if file.startswith(rel_path):
                     src_file = os.path.join(base_dir, file)
-                    dest_dir = os.path.join(dest_dir, rel_path, file)
-                    shutil.move(src_file, dest_dir)
+                    dest_dir = os.path.join(original_dest_dir, rel_path, file + "_" + source)
+                    if os.path.isdir(src_file):
+                        shutil.copytree(src_file, dest_dir)
+                    else:
+                        shutil.copy2(src_file, dest_dir)
+                    print(f"{src_file} to {dest_dir} finish")
 
 def shuffle(directory):
     dest_dir = os.path.join(directory, "shuffle")
@@ -50,20 +59,24 @@ def shuffle(directory):
     move_matching_files(dest_dir, input_rust_directory, all_i_files, except_ir = True)
 
     input_c_ir = os.path.join(directory, "klee_ir_files", "C")
-    move_matching_files(dest_dir, input_c_ir, all_i_files)
+    move_matching_files(dest_dir, input_c_ir, all_i_files, source = "C")
 
     input_rust_ir = os.path.join(directory, "klee_ir_files", "Rust")
-    move_matching_files(dest_dir, input_rust_ir, all_i_files)
+    move_matching_files(dest_dir, input_rust_ir, all_i_files, source = "Rust")
 
     input_c_symbol_log = os.path.join(directory, "klee_symbol_log", "C")
-    move_matching_files(dest_dir, input_c_symbol_log, all_i_files)
+    move_matching_files(dest_dir, input_c_symbol_log, all_i_files, source = "C")
 
     input_rust_symbol_log = os.path.join(directory, "klee_symbol_log", "Rust")
-    move_matching_files(dest_dir, input_rust_symbol_log, all_i_files)
+    move_matching_files(dest_dir, input_rust_symbol_log, all_i_files, source = "Rust")
 
-    input_output_directory = os.path.join(directory, "graph_output", "C")
-    move_matching_files(dest_dir, input_output_directory, all_i_files)
+    input_c_output_directory = os.path.join(directory, "graph_output", "C")
+    move_matching_files(dest_dir, input_c_output_directory, all_i_files, source = "C")
+
+    input_rust_output_directory = os.path.join(directory, "graph_output", "Rust")
+    move_matching_files(dest_dir, input_rust_output_directory, all_i_files, source = "Rust")
 
 
 if __name__ == "__main__":
-    shuffle("../Symbolizer/tempcase")
+    # shuffle("../Symbolizer/optipng_gpt_4o_20250412_2025-04-12_05-11-08")
+    shuffle(sys.argv[1])
